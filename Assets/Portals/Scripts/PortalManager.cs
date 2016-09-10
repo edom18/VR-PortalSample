9238d3d9740f2bfbea96e2fa7893d759e340730f
edo_m18 <edo.m18@gmail.com>
2016-09-11 03:50:56 +0900

    Translate JS to C#.

---
+++ Assets/Portals/Scripts/PortalManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalManager : MonoBehaviour {
+
+    public GameObject[] RenderTextureCameras;
+    public GameObject[] Portals;
+
+    int _portalIndex;
+	
+	// Update is called once per frame
+	void Update () {
+        _portalIndex = 0;
+
+        foreach(var camera in RenderTextureCameras) {
+            camera.transform.localPosition = transform.position - Portals[_portalIndex].transform.position;
+            camera.transform.localRotation = transform.localRotation;
+            _portalIndex++;
+        }
+	}
+}
