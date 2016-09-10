03569c5f5394c63d5218cc038fa64610a65b64c4
edo_m18 <edo.m18@gmail.com>
2016-09-11 04:04:38 +0900

    Added mover for camera rig.

---
+++ Assets/Portals/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public Transform CameraRig;
+
+    float _speed = 0.05f;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            var pos = transform.position;
+            pos += CameraRig.forward * _speed;
+            transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            var pos = transform.position;
+            pos -= CameraRig.forward * _speed;
+            transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            var pos = transform.position;
+            pos += CameraRig.right * _speed;
+            transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            var pos = transform.position;
+            pos -= CameraRig.right * _speed;
+            transform.position = pos;
+        }
+	}
+}
